c99a109593aa23980c07c152fcc8db1f59c72982
Jacawb <jacobmadison8@gmail.com>
2026-02-21 17:32:14 -0800

    changed the gamemanager.cs code

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,18 +2,45 @@ using UnityEngine;
 using TMPro;
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] TMP_Text countText;
+    [Header("Stats")]
+    public double brainPower = 0;
 
-    int count = 0;
+    [Header("Objects")]
+    public TextMeshProUGUI bpText;
+    public GameObject brainPowerTextObject;
+    public GameObject upgradeMenu;
 
-    public void ClickAction()
+    [Header("Settings")]
+    public double menuThreshold = 15;
+    public double textThreshold = 10;
+
+    void Start()
     {
-        count++;
+        brainPowerTextObject.SetActive(false);
+        upgradeMenu.SetActive(false);
         UpdateUI();
     }
 
+    public void ClickAction()
+    {
+        brainPower += 1;
+        UpdateUI();
+        CheckThresholds();
+    }
+
+    void CheckThresholds()
+    {
+        if (brainPower >= textThreshold)
+        {
+            brainPowerTextObject.SetActive(true);
+        }
+        if (brainPower >= menuThreshold)
+        {
+            upgradeMenu.SetActive(true);
+        }
+    }
     void UpdateUI()
     {
-        countText.text = count.ToString();
+        bpText.text = "Brain Power: " + brainPower.ToString("F0");
     }
 }
