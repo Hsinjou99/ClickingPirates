53a63d8f5940cb7e52ca029f5309ae9e9bb83ac4
Jacawb <jacobmadison8@gmail.com>
2026-02-21 18:54:21 -0800

    added passive income upgrade

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,12 +3,27 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     [Header("Stats")]
+    // currency
     public double brainPower = 0;
+    // passive income value
+    public double bpPerSecond = 0;
+    // upgrade multipliers
+    public float multiplier = 1f;
 
     [Header("Objects")]
     public TextMeshProUGUI bpText;
     public GameObject brainPowerTextObject;
     public GameObject upgradeMenu;
+
+    [Header("Upgrades")]
+    public TextMeshProUGUI upgrade2Text;
+    public TextMeshProUGUI upgrade3Text;
+    public TextMeshProUGUI upgrade4Text;
+
+    [Header("Upgrade Costs")]
+    public double upgrade2Cost = 10;
+    public double upgrade3Cost = 50;
+    public double upgrade4Cost = 100;
 
     [Header("Settings")]
     public double menuThreshold = 15;
@@ -21,11 +36,36 @@ public class GameManager : MonoBehaviour
         UpdateUI();
     }
 
+    void Update()
+    {
+        if (bpPerSecond > 0)
+        {
+            brainPower += bpPerSecond * Time.deltaTime;
+            UpdateUI();
+            CheckThresholds();
+        }
+    }
+
+    // clicking the rock
     public void ClickAction()
     {
         brainPower += 1;
         UpdateUI();
         CheckThresholds();
+    }
+
+    // upgrade buttons
+
+    // upgrade 2: passive income
+    public void BuyUpgrade2()
+    {
+        if (brainPower >= upgrade2Cost)
+        {
+            brainPower -= upgrade2Cost;
+            bpPerSecond += 2;
+            upgrade2Cost *= 1.4;
+            UpdateUI();
+        }
     }
 
     void CheckThresholds()
@@ -42,5 +82,8 @@ public class GameManager : MonoBehaviour
     void UpdateUI()
     {
         bpText.text = "Brain Power: " + brainPower.ToString("F0");
+
+        // update upgrade buttons
+        if (upgrade2Text) upgrade2Text.text = $"Upgrade 2 ({upgrade2Cost.ToString("F0")} BP)";
     }
 }
